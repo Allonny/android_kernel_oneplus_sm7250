dfc121c65d75bab66169be4e3cd392bc9dd25864
Allonny <37370681+Allonny@users.noreply.github.com>
2025-07-27 16:13:21 +0500

    Update huge_memory.c
    pmd_t _pmd removed

--- mm/huge_memory.c
+++ mm/huge_memory.c
@@ -2290,7 +2290,7 @@ void __split_huge_pmd(struct vm_area_struct *vma, pmd_t *pmd,
 	struct mm_struct *mm = vma->vm_mm;
 	unsigned long haddr = address & HPAGE_PMD_MASK;
 	bool do_unlock_page = false;
-	pmd_t _pmd;
+	// pmd_t _pmd;
 
 	mmu_notifier_invalidate_range_start(mm, haddr, haddr + HPAGE_PMD_SIZE);
 	ptl = pmd_lock(mm, pmd);
